5089b817ade0524c0d35606319ac0433a67248a6
samson vang <vangsam2@gmail.com>
2021-07-07 01:05:10 -0500

    created objecetive and working on adding albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,12 @@
 console.log('***** Music Collection *****')
+let collection = {};
+function addToCollection( title, artist, yearPublished ){
+  addToCollection.title = 'title';
+  addToCollection.artist = 'artist';
+  addToCollection.yearPublished = 'yearPublished';
+  addToCollection.push = {};
+  return addToCollection;
+}
+console.log(collection)
+addToCollection.title = 'Fire on fire'
+console.log('Collection:', collecton)
