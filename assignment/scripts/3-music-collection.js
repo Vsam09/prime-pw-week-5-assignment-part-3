665c5fb1b1c022403e7f3ae033e1baeb833e3fb5
samson vang <vangsam2@gmail.com>
2021-07-07 18:36:25 -0500

    showCollection complete

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -5,14 +5,25 @@ function addToCollection (title, artist, yearPublished) {
   album.title = title;
   album.artist = artist;
   album.yearPublished = yearPublished;
+  //Add the new object to the end of the `collection` array
   collection.push(album);
+  //Return the newly created object
   return album;
 }
-
+//Console.log each album as added using the returned value
 console.log(' Added Album01', collection.album01 = addToCollection('Fire on fire', 'Sam Smith', 2018) );
 console.log(' Added Album02', collection.album02 = addToCollection('No matter what', 'Calum Scott', 2018) );
 console.log(' Added Album03', collection.album03 = addToCollection('You are the reason', 'Calum Scott', 2018) );
 console.log(' Added Album04', collection.album04 = addToCollection('I belong to you', 'Jacob Lee', 2017) );
 console.log(' Added Album05', collection.album05 = addToCollection('Talking to the moon', 'Bruno Mars', 2010) );
 console.log(' Added Album06', collection.album06 = addToCollection('Five more minutes', 'Scotty McCreery', 2018) );
+//After all are added, console.log the `collection` array
 console.log('Collection', collection)
+
+function showCollection( array ){
+  console.log('There are albums in this array', array.length);
+  for ( let items of array ){
+  console.log(`${items.title} by ${items.artist} published in ${items.yearPublished}`);
+  }
+}
+  showCollection (collection);
