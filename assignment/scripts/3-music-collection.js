ed67c79397239111e10c9deb1a76d68a5326c7d7
samson vang <vangsam2@gmail.com>
2021-07-07 14:58:32 -0500

    updated object / function to operate correctly

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,12 +1,19 @@
 console.log('***** Music Collection *****')
-let collection = {};
-function addToCollection( title, artist, yearPublished ){
-  addToCollection.title = 'title';
-  addToCollection.artist = 'artist';
-  addToCollection.yearPublished = 'yearPublished';
-  addToCollection.push = {};
-  return addToCollection;
+let collection = [];
+function addToCollection (title, artist, yearPublished) {
+  const album = {};
+  album.title = title;
+  album.artist = artist;
+  album.yearPublished = yearPublished;
+  collection.push(album);
+  return collection;
 }
-console.log(collection)
-addToCollection.title = 'Fire on fire'
-console.log('Collection:', collecton)
+
+collection.album = addToCollection('Fire on fire', 'Sam Smith', 2020);
+collection.album = addToCollection('No matter what', 'Calum Scott', 2018);
+collection.album = addToCollection('You are the reason', 'Calum Scott', 2018);
+collection.album = addToCollection('I belong to you', 'Jacob Lee', 2017);
+collection.album = addToCollection('Talking to the moon', 'Bruno Mars', 2010);
+collection.album = addToCollection('Five more minutes', 'Scotty McCreery', 2018);
+
+console.log('Collection', collection)
