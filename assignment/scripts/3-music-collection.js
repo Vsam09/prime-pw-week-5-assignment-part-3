27f2dc4f55260d0143bc867908b76ea93b51c507
samson vang <vangsam2@gmail.com>
2021-07-07 16:19:37 -0500

    Fixed object to functionand return correctly

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -6,14 +6,13 @@ function addToCollection (title, artist, yearPublished) {
   album.artist = artist;
   album.yearPublished = yearPublished;
   collection.push(album);
-  return collection;
+  return album;
 }
 
-collection.album = addToCollection('Fire on fire', 'Sam Smith', 2020);
-collection.album = addToCollection('No matter what', 'Calum Scott', 2018);
-collection.album = addToCollection('You are the reason', 'Calum Scott', 2018);
-collection.album = addToCollection('I belong to you', 'Jacob Lee', 2017);
-collection.album = addToCollection('Talking to the moon', 'Bruno Mars', 2010);
-collection.album = addToCollection('Five more minutes', 'Scotty McCreery', 2018);
-
+console.log(' Added Album01', collection.album01 = addToCollection('Fire on fire', 'Sam Smith', 2018) );
+console.log(' Added Album02', collection.album02 = addToCollection('No matter what', 'Calum Scott', 2018) );
+console.log(' Added Album03', collection.album03 = addToCollection('You are the reason', 'Calum Scott', 2018) );
+console.log(' Added Album04', collection.album04 = addToCollection('I belong to you', 'Jacob Lee', 2017) );
+console.log(' Added Album05', collection.album05 = addToCollection('Talking to the moon', 'Bruno Mars', 2010) );
+console.log(' Added Album06', collection.album06 = addToCollection('Five more minutes', 'Scotty McCreery', 2018) );
 console.log('Collection', collection)
